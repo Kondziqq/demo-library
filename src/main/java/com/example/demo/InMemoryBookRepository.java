56dc87ed88ab922cd20a94741a04edd5bd6981e3
Bartosz Pędziwiatr <b.pedziwiatr93@gmail.com>
2023-09-30 13:45:41 +0200

    My first homework

--- src/main/java/com/example/demo/InMemoryBookRepository.java
+++ src/main/java/com/example/demo/InMemoryBookRepository.java
@@ -5,19 +5,21 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
-class InMemoryBookRepository {
+class InMemoryBookRepository  {
     private List<BookEntity> books = new ArrayList<>(List.of(
             BookEntity.builder().id(1L).title("W pustyni i w puszczy").author("Henryk Sienkiewicz").build(),
-            BookEntity.builder().id(2L).title("Ojciec Chrzestny").author("Mario Puzo").build()
+            BookEntity.builder().id(2L).title("Ojciec Chrzestny").author("Mario Puzo").build(),
+            BookEntity.builder().id(3L).title("Lalka").author("Bolesław Prus").build()
     ));
 
     public List<BookEntity> findAll() {
         return books;
     }
 
-    BookEntity findById(Long id) {
+    public BookEntity findById(Long id) {
         return books.stream().filter(book -> id.equals(book.getId())).findFirst().orElseThrow();
     }
 
@@ -26,4 +28,12 @@ class InMemoryBookRepository {
         book.setId(++newId);
         books.add(book);
     }
+    public List<BookEntity> findByAuthor(String author) {
+        return books.stream().filter(book -> author.equals(book.getAuthor())).collect(Collectors.toList());
+    }
+    public void deleteById (Long id){
+        BookEntity bookToBeDeleted = books.stream().filter(book -> id.equals(book.getId())).findFirst().orElseThrow();
+        books.remove(bookToBeDeleted);
+    }
+
 }
